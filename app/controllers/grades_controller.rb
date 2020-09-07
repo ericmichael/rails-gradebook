be10ce2cea2ab12f70c8c894d22db4d1446d29c4
Liam San Pedro <liam.sanpedro01@utrgv.edu>
2020-09-07 15:21:53 -0500

    added before_action function for authentication to remove repetitive code

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,72 +1,48 @@
 class GradesController < ApplicationController
   before_action :set_grade, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user
 
   # GET /grades
   def index
-    if !user_signed_in?
-      redirect_to user_session_path
-    end
   end
 
   # GET /grades/1
   def show
-    if !user_signed_in?
-      redirect_to user_session_path
-    end
   end
 
   # GET /grades/new
   def new
-    if !user_signed_in?
-      redirect_to user_session_path
-    else
-      @grade = Grade.new
-    end
+    @grade = Grade.new
   end
 
   # GET /grades/1/edit
   def edit
-    if !user_signed_in?
-      redirect_to user_session_path
-    end
   end
 
   # POST /grades
   def create
-    if !user_signed_in?
-      redirect_to user_session_path
-    else
-      @grade = Grade.new(grade_params)
+    @grade = Grade.new(grade_params)
 
-      if @grade.save
-        redirect_to @grade, notice: 'Grade was successfully created.'
-      else
-        render :new
-      end
+    if @grade.save
+      redirect_to @grade, notice: 'Grade was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /grades/1
   def update
-    if !user_signed_in?
-      redirect_to user_session_path
+    if @grade.update(grade_params)
+      redirect_to @grade, notice: 'Grade was successfully updated.'
     else
-      if @grade.update(grade_params)
-        redirect_to @grade, notice: 'Grade was successfully updated.'
-      else
-        render :edit
-      end
+      render :edit
     end
   end
 
   # DELETE /grades/1
   def destroy
-    if !user_signed_in?
-      redirect_to user_session_path
-    else
-      @grade.destroy
-      redirect_to grades_url, notice: 'Grade was successfully destroyed.'
-    end
+    @grade.destroy
+    redirect_to grades_url, notice: 'Grade was successfully destroyed.'
   end
 
   private
@@ -78,5 +54,11 @@ class GradesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def grade_params
       params.require(:grade).permit(:student_id, :student_name, :student_grade)
+    end
+
+    def authenticate_user
+      if !user_signed_in?
+        redirect_to user_session_path
+      end
     end
 end
