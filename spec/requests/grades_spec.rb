7a66caae66a138b4258e77f6c11c5d38bf885274
Damon Mohan <damon@Damons-MacBook-Pro.local>
2020-09-13 20:21:05 -0500

    added GET request sad paths

--- spec/requests/grades_spec.rb
+++ spec/requests/grades_spec.rb
@@ -129,6 +129,24 @@ RSpec.describe "/grades", type: :request do
       get grades_url
       expect(response).to redirect_to(new_user_session_path)
     end
+
+    it "should not GET /grades/:id/" do
+      grade = create(:grade, :valid)
+      get grade_url(grade)
+      expect(response).to redirect_to(new_user_session_path)
+
+    end
+    it "should not GET /grades/new" do
+      get new_grade_url
+      expect(response).to redirect_to(new_user_session_path)
+    end
+    it "should not GET /grades/:id/edit" do
+      grade = create(:grade, :valid)
+      get edit_grade_url(grade)
+      expect(response).to redirect_to(new_user_session_path)
+
+    end
+
     it "should not POST /create" do
       post grades_url, params: { grade: attributes_for(:grade, :valid) }
       expect(response).to redirect_to(new_user_session_path)
